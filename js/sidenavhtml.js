79f387eaf6711e459609d61c7f6b8c938ee312a6
Aiden G <orca950@gmail.com>
2017-04-28 13:49:03 -0500

    moved sidenav html to a js file

---
+++ js/sidenavhtml.js
@@ -0,0 +1,9 @@
+document.write('\
+	<div id="mySidenav" class="sidenav" align="left">\
+		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>\
+		<a href="index.html">Home</a>\
+		<a href="#">Links</a>\
+		<a href="#">Dowloads</a>\
+	</div>\
+	<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>\
+	');
