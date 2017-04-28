426db15cfc86ca13717b32bd47c2d72c635a1f5c
Aiden G <orca950@gmail.com>
2017-04-28 14:14:23 -0500

    bug fixes

--- js/sidenavhtml.js
+++ js/sidenavhtml.js
@@ -1,9 +1,36 @@
+var fileName = location.href.split("/").slice(-1);
+var link0;
+var link1;
+var link2;
+var path0;
+var path1;
+var path2;
+
+if (fileName == "index.html") {
+	link0 = "Links";
+	link1 = "Downloads";
+	link2 = "Changelog";
+	
+	path0 = "#";
+	path1 = "#";
+	path2 = "changelog.html";
+}
+if (fileName == "changelog.html") {
+	link0 = "Home";
+	link1 = "Links";
+	link2 = "Downloads";
+	
+	path0 = "index.html";
+	path1 = "#";
+	path2 = "#";
+}
+
 document.write('\
 	<div id="mySidenav" class="sidenav" align="left">\
 		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>\
-		<a href="index.html">Home</a>\
-		<a href="#">Links</a>\
-		<a href="#">Dowloads</a>\
+		<a href=" '+ path0 +' ">'+ link0 +'</a>\
+		<a href="'+ path1 +'">'+ link1 +'</a>\
+		<a href="'+ path2 +'">'+ link2 +'</a>\
 	</div>\
 	<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>\
 	');
